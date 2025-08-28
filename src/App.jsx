6107a8a470af1392886d993a87e4da44de3afbe3
EmonMridha <emonmridha712@gmail.com>
2025-08-28 21:40:16 +0600

    first

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import DaisyNav from './components/DaisyNav/DaisyNav'
+import NavBar from './components/Navbar/NavBar'
+
+function App() {
+
+
+  return (
+    <>
+
+    <header>
+      <NavBar></NavBar>
+      <DaisyNav></DaisyNav>
+    </header>
+    <main>
+
+    </main>
+    </> 
+  )
+}
+
+export default App
